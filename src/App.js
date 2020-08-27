3409b4b08018713cfcde84c933c36f6e270911b8
tjuranek <thomas@juranek.com>
2020-08-26 19:43:31 -0500

    display weather data

--- src/App.js
+++ src/App.js
@@ -3,17 +3,41 @@ import { getWeatherForLocation } from "./services";
 
 export const App = () => {
   const [state, setState] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const getWeather = async () => {
       const data = await getWeatherForLocation("Roseville", "Minnesota");
-      debugger;
-      console.log(data);
-      setState(data);
+      setState({ ...data });
+      setIsLoading(false);
     };
 
     getWeather();
   }, []);
 
-  return <div>hi</div>;
+  return (
+    <>
+      {isLoading ? (
+        <>loading</>
+      ) : (
+        <>
+          <h5>Current</h5>
+          <div>Current: {state.currentConditions.tempCurrent}</div>
+          <div>High: {state.currentConditions.tempHigh}</div>
+          <div>Low: {state.currentConditions.tempLow}</div>
+          <div>Description: {state.currentConditions.weatherDescription}</div>
+
+          <br />
+
+          <h5>Forecast</h5>
+          <div>Day One High: {state.forecast[0].tempHigh}</div>
+          <div>Day One Low: {state.forecast[0].tempLow}</div>
+          <div>Day One Description: {state.forecast[0].weatherDescription}</div>
+          <div>Day Two High: {state.forecast[1].tempHigh}</div>
+          <div>Day Two Low: {state.forecast[1].tempLow}</div>
+          <div>Day Two Description: {state.forecast[1].weatherDescription}</div>
+        </>
+      )}
+    </>
+  );
 };
