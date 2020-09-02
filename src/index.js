baa8adf9886f7c4209f5b7c2aadcdb37a0fdeb5a
tjuranek <thomas@juranek.com>
2020-09-02 17:03:45 -0500

    add back create-react-app service worker

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./app";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from './app';
+import * as serviceWorker from './service-worker';
 
 ReactDOM.render(
 	<React.StrictMode>
 		<App />
 	</React.StrictMode>,
-	document.getElementById("root")
+	document.getElementById('root')
 );
+
+serviceWorker.register();
