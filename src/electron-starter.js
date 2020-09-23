3aa0de1ef3afc16bd8ce4e8f790be8bd650992fa
tjuranek <thomas@juranek.com>
2020-09-22 21:27:45 -0500

    add electron-starter

---
+++ src/electron-starter.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+	const window = new BrowserWindow({
+		width: 800,
+		height: 600
+	});
+
+	window.loadURL('http://localhost:3000/');
+};
+
+app.on('ready', () => {
+	createWindow();
+});
+
+app.on('activate', () => {
+	if (BrowserWindow.getAllWindows().length === 0) {
+		createWindow();
+	}
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') {
+		app.quit();
+	}
+});
